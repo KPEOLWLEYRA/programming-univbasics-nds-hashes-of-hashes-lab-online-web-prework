a11e601577ddb3ee3d17efe29620196086cb44ad
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 23:29:31 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,25 +43,22 @@ def naming_system
   # HoH all the way to Species!
   naming_system = {
     kingdom = {
-      :label => "Kingdom" => {
-        :sub_category => {
-          :label => "Phylum" => {
-            :sub_category => {
+      :label => "Kingdom", 
+      :sub_category => {
+          :label => "Phylum",  
+          :sub_category => {
             :label => "Class"
             }
           }
         }
-      }
-    }
   :label => "Order"
-  :label => "Family" => {
+  :label => "Family", 
     :sub_category => {
       :label => "Genus"
     }
   }
-  :label => "Species" = {
+  :label => "Species", 
     :sub_category => nil 
-  }
 }
 naming_system
 end
